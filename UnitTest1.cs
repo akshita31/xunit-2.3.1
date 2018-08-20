4527375e6294f792aeb171022133556032a18eff
Akshita Agarwal <akagarw@microsoft.com>
2018-08-20 11:57:44 -0700

    xunit test project

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace xunit2._3._1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
